f34f4d80be5409edb26c647dcf5f49581f380a52
Yorinton <ka2ki.yori@outlook.com>
2017-08-25 14:52:07 +0000

    fix style of rooms.blade.php

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -4,25 +4,29 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h1>メッセージ一覧</h1>
             @if(isset($friends))
             @foreach($friends as $friend)
             <div class="matching_friend">
-                <div>
-                    <p>{{ $friend->name }}</p>
-                    <p>
-                        @foreach($friend->idols as $idol)
-                        <span>{{ $idol->idol }}</span>
-                        @endforeach
-                        <span>他</span>
-                    </p>
-                    <form method="post" action="{{ url('/room') }}">
+                <!-- <a> -->
+                <div class="of_h pt10 pb10 list_border to_room">
+                    <img class="thumb_mini" src="{{ $friend->img_path}}">
+                    <div class="fr wd80">
+                        <p class="mb0">{{ $friend->name }}</p>
+                        <p class="mb0">20代前半 / {{ $friend->sex}} / 
+                            @foreach($friend->regions as $region)
+                            <span>{{ $region->region }}</span>
+                            @endforeach
+                        </p>
+                        <p class="mb0 text_clamp_s">{{ $friend->introduction}}</p>
+                    </div>
+                    <form method="post" action="{{ url('/room') }}" class="disnone make_room">
                         {{ csrf_field() }}
                         <input type="hidden" name="from_user_id" value="{{ Auth::id() }}">
                         <input type="hidden" name="to_user_id" value="{{ $friend->id }}">
                         <input type="submit" name="" value="メッセージを送る">
                     </form>
                 </div>
+                <!-- </a> -->
             </div>
             @endforeach
             @elseif(isset($m))
