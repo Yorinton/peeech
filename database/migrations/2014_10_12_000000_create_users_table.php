e8f235fdad17f870f80143264b7c2b86fb243eb9
Yorinton <ka2ki.yori@outlook.com>
2017-08-30 17:43:05 +0000

    change migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',255);
-            $table->string('email',255);
+            $table->string('email')->unique();
             $table->string('sex',11);
             $table->date('birthday');
             $table->string('img_path',255);
