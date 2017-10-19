2f9667fa05e2bf23efdd2af1d93f5b70890977e5
Yorinton <ka2ki.yori@outlook.com>
2017-10-19 14:33:34 +0000

    fix MatchingService/notifyMatching method using sub query

--- app/MatchingService.php
+++ app/MatchingService.php
@@ -30,25 +30,22 @@ class MatchingService
         if(User::all()){
             // 全Userを取得
             $users = User::all();
+
             foreach ($users as $user) {
                 DB::beginTransaction();
                 try {
-                    $matchings = Matching::where('from_user_id', $user->id)->where('judge', 1)->get();
-                    $friends = [];
-                    foreach ($matchings as $matching) {
-                        if (Matching::where('from_user_id', $matching->to_user_id)->where('to_user_id', $user->id)->where('judge', 1)->exists()) {
-                            if ($matching->settled === null) {
-                                $friend = User::where('id', $matching->to_user_id)->first();
-                                $friends[] = $friend;
-                                $matching->settled = true;
-                                $matching->save();
-                            }
-                        }
-                    }
+
+                    $friends = User::where('id',DB::raw("any(select from_user_id from matchings where from_user_id = any(select to_user_id from matchings where from_user_id = $user->id and judge = 1 and settled is null) and to_user_id = $user->id and judge = 1)"))->get();
                     // 配列を操作する際はその配列の要素が１つ以上存在するかチェックしてから
+
                     if (count($friends) > 0) {
+                        foreach($friends as $friend){
+                            $matching = Matching::where('from_user_id',$user->id)->where('to_user_id',$friend->id)->first();
+                            $matching->settled = 1;
+                            $matching->save();
+                        }
                         $friends_num = count($friends);
-                        $friend_ex = $friends[0];
+                        $friend_ex = $friends->first();
 
                         // MatchingNotificationインスタンスの引数にファン友数とファン友の一人のインスタンスと自分のインスタンスを渡す
                         Mail::to(decrypt($user->email))->send(new MatchingNotification($friends_num, $friend_ex, $user));
