af8410ac66f1e410d7f590d15456dac1ca9e84c3
Yorinton <ka2ki.yori@outlook.com>
2017-10-23 17:59:02 +0000

    make recommendNotification Mailable

--- tests/Unit/MailTest.php
+++ tests/Unit/MailTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Eloquent\User;
 
 use App\Mail\MatchingNotification;
+use App\Mail\RecommendNotification;
 use Illuminate\Support\Facades\Mail;
 
 class MailTest extends TestCase
@@ -25,5 +26,13 @@ class MailTest extends TestCase
     	$friend_ex = User::where('id',3)->first();
     	$user = User::where('id',1)->first();
         Mail::to('sansan106700@gmail.com')->send(new MatchingNotification($friends_num,$friend_ex,$user));
+        $this->assertTrue(true);
+    }
+
+    public function testRecommendNotification()
+    {
+        $user = User::where('id',1)->first();
+        Mail::to(decrypt($user->email))->send(new RecommendNotification());
+        $this->assertTrue(true);
     }
 }
