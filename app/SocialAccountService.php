be6fa67f9d1a3a4db8c1598b5c3b8e3c4690dde3
Yorinton <ka2ki.yori@outlook.com>
2017-10-25 16:44:34 +0000

    fix SocialAcountService

--- app/SocialAccountService.php
+++ app/SocialAccountService.php
@@ -16,28 +16,43 @@ class SocialAccountService
 	**/
 	public function findOrCreate(ProviderUser $providerUser,$provider)
 	{
-        //SNSアカウントのemailに紐づくUserインスタンスを取得
-        $user = User::where('email',$providerUser->getEmail())->first();
+        //SNSアカウントを取得
+        $account = LinkedSocialAccount::where('provider_name',$provider)
+            ->where('provider_id',$providerUser->getId())
+            ->first();
 
-        //SNSアカウントのemailにひもづくUserインスタンスが無い場合
-        if(!$user){
-            //SNSアカウントのemailとnameを使ってUserインスタンスを作成しテーブルに保存
-            $email = encrypt($providerUser->getEmail());
-            $user = new User();
-            $user->email = $email;
-            $user->name = $providerUser->getName();
-            $user->img_path = $providerUser->getAvatar();
-            $user->save();
-        }
+        //SNSアカウントが存在する場合、関連するUserインスタンスを取得(アカウント作成済み = ログイン)
+        if($account){
+            $user = $account->user;
 
-        //Userに関連するLinkedSocialAccountインスタンスを作成
-        $user->accounts()->create([
+            // ステータスとユーザーを返す
+            $result = array('login',$user);
+            return $result;
+        }else {
+
+            //SNSアカウントのemailに紐づくUserインスタンスを取得
+            $user = User::where('email', $providerUser->getEmail())->first();
+
+            //SNSアカウントのemailにひもづくUserインスタンスが無い場合
+            if (!$user) {
+                //SNSアカウントのemailとnameを使ってUserインスタンスを作成しテーブルに保存
+                $email = encrypt($providerUser->getEmail());
+                $user = new User();
+                $user->email = $email;
+                $user->name = $providerUser->getName();
+                $user->img_path = $providerUser->getAvatar();
+                $user->save();
+            }
+
+            //Userに関連するLinkedSocialAccountインスタンスを作成
+            $user->accounts()->create([
                 'provider_id' => $providerUser->getId(),
                 'provider_name' => $provider,
             ]);
 
-        $result = array('register',$user);
-        return $result;
+            $result = array('register', $user);
+            return $result;
+        }
 
     }
 }
