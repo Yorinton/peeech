6ed53c7bf0fb824ac134aeaa434df3885dbcb7c9
Yorinton <ka2ki.yori@outlook.com>
2017-10-26 09:24:57 +0000

    fix mathed_plain.blade.php

--- resources/views/emails/matched_plain.blade.php
+++ resources/views/emails/matched_plain.blade.php
@@ -1,21 +1,21 @@
-<body>
-<div class="container">
-    <h3><img class="catch_img" style="width:100%;" src="{{ $message->embed(url('/images/services/match.png')) }}"></h3>
-    <p>{{ $user->name }}さんへ</p>
-    <p class="pd5">新しく{{ $friends_num }}人とマッチング!<br>
-        気の合いそうな人を見つけてメッセを送ってみましょう♬</p>
-    <div class="friend_box">
-        <p>
-            <span>{{ $friend_ex->name }}</span><br>
-        </p>
-        <p class="pd5">
-            @foreach($friend_ex->idols as $idol)
-                <span class="tag_pink mb5 mr5 dis_inblo">{{ $idol->idol }}</span><br>
-            @endforeach
-        </p>
-        <span class="introduction">{{ $friend_ex->introduction }}</span>
-        </p>
-    </div>
-    <p><a class="btn_link" href="{{ $url }}">他のマッチングをチェック♬</a></p>
-</div>
-</body>
+{{ $user->name }}さんへ
+
+マッチングおめでとうございます！
+
+新しく{{ $friends_num }}人とマッチング!
+気の合いそうな人を見つけてメッセを送ってみましょう♬
+
+＜ファン友候補(1人目)＞
+ニックネーム：{{ $friend_ex->name }}
+好きなアイドル：
+@foreach($friend_ex->idols as $idol)
+    {{ $idol->idol }}
+@endforeach
+自己紹介文：
+{{ $friend_ex->introduction }}
+
+↓↓↓マッチングしたファン友候補をチェック♬↓↓↓
+{{ $url }}">
+
+
+Peeech.com
