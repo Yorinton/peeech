955b685f8b5b176687137cadf4e90015d9ec9f3a
Yorinton <ka2ki.yori@outlook.com>
2017-10-29 11:06:32 +0000

    add 403 error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mb30 mt50">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        	<h2>403 Forbidden</h2>
+        	<p>アクセス権がありません</p>
+		</div>
+	</div>
+</div>
+@endsection
