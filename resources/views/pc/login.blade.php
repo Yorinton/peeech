43413001a9b751bd4e2dc2f727465635c3ad2263
Yorinton <ka2ki.yori@outlook.com>
2017-11-27 16:29:48 +0000

    PCページ全体スタイル調整

--- resources/views/pc/login.blade.php
+++ resources/views/pc/login.blade.php
@@ -21,89 +21,97 @@
                 </div>
             </header>
             <section class="convert_to_max_size_of_page section_bg_color fc_white pl15 pr15 pt30">
-                <h2 class="mt0 pt20 fs24 fw_b mb30">アイドルファン活動をより楽しくする出会い</h2>
+                <h2 class="mt30 pt20 fs40 fw_b mb10">アイドルファン活動をより楽しくする出会い</h2>
                 <div class="mb30">
-                    <p class="mb30">Peeechは、アイドルファンの「ファン同士でつながりたい」「一緒にこんなことをしてみたい」を実現し、ファン活動がより充実・楽しくなるアイドルファンに特化した友達作りサービスです。</p>
+                    <p class="mb30">Peeechは、アイドルファンの「ファン同士でつながりたい」「一緒にこんなことをしてみたい」を実現し、<br>ファン活動がより充実・楽しくなるアイドルファンに特化した友達作りサービスです。</p>
                 </div>
                 <div>
-                    <img class="wd90 mb20" src="../../images/top/second_01.png">
+                    <img class="wd50 mb20" src="../../images/top/second_01.png">
                 </div>
             </section>
             <section>
-                <h3 class="fw_b mb35 mt35">こんなことありませんか？</h3>                
-                <div class="mb50">
-                    <img class="wd60 img_box_shadow" src="../../images/top/needs_01.png">
-                    <p class="mt25 fs16 fw_b">共通点があって気の合うファン友と<br>一緒にファン活動を楽しみたい</p>
+                <h3 class="fw_b mb60 mt60 fs40">こんなことありませんか？</h3>
+                <div class="sp-around">
+                    <div class="mb50">
+                        <img class="wd60 img_box_shadow" src="../../images/top/needs_01.png">
+                        <p class="mt25 fs16 fw_b">共通点があって気の合うファン友と<br>一緒にファン活動を楽しみたい</p>
+                    </div>
+                    <div class="mb50">
+                        <img class="wd60 img_box_shadow" src="../../images/top/needs_02.png">
+                        <p class="mt25 fs16 fw_b">ライブや握手会楽しみ！<br>でもぼっち参加でちょっと寂しい</p>
+                    </div>
+                    <div class="mb50">
+                        <img class="wd60 img_box_shadow" src="../../images/top/needs_03.png">
+                        <p class="mt25 fs16 fw_b">友達募集や同行者募集するけど<br>中々声がかからない</p>
+                    </div>
                 </div>
-                <div class="mb50">
-                    <img class="wd60 img_box_shadow" src="../../images/top/needs_02.png">
-                    <p class="mt25 fs16 fw_b">ライブや握手会楽しみ！<br>でもぼっち参加でちょっと寂しい</p>
-                </div>
-                <div class="mb50">
-                    <img class="wd60 img_box_shadow" src="../../images/top/needs_03.png">
-                    <p class="mt25 fs16 fw_b">友達募集や同行者募集するけど<br>中々声がかからない</p>
-                </div>                
             </section>
-            <section class="convert_to_max_size_of_page section_bg_color fc_white pl15 pr15 pb5">
-                <h3 class="mb35 pt35 fw_b">Peeechなら</h3>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/miryoku_01.png">
-                    <h4 class="fw_b mt20 mb20">あなたに合った<br>ファン友を見つけられる</h4>
-                    <p class="mb40 pl15 pr15">プロフィールを解析し同じあなたに合ったファン仲間を自動でレコメンドします。待っているだけでファン友候補を見つけることが出来ます^^</p>
-                </div>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/miryoku_02.png">
-                    <h4 class="fw_b mt20 mb20">気の合ったファン友の中から<br>イベント同行者を探せる</h4>
-                    <p class="mb40 pl15 pr15">普段から絡めるファン友を気軽に見つけられます。普段からアイドルトークで盛り上がったり、一緒にお茶したりしながら仲良くなればイベントやライブにも誘いやすくなります^^</p>
-                </div>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/miryoku_03.png">
-                    <h4 class="fw_b mt20 mb20">アイドルファン特化だから<br>友達や同行者を見つけやすい</h4>
-                    <p class="mb40 pl15 pr15">通常のマッチングサービスやイベント同行者募集サービスと異なりアイドルファン特化型のため効率的にファン仲間と繋がれます</p>
+            <section class="convert_to_max_size_of_page section_bg_color fc_white pl15 pr15 pb5 pt5">
+                <h3 class="mb60 mt60 fw_b fs40">Peeechなら</h3>
+                <div class="sp-around">
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/miryoku_01.png">
+                        <h4 class="fw_b mt20 mb20">あなたに合った<br>ファン友を見つけられる</h4>
+                        <p class="mb40 pl15 pr15 explain_80">プロフィールを解析し同じあなたに合ったファン仲間を自動でレコメンドします。待っているだけでファン友候補を見つけることが出来ます^^</p>
+                    </div>
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/miryoku_02.png">
+                        <h4 class="fw_b mt20 mb20">気の合ったファン友の中から<br>イベント同行者を探せる</h4>
+                        <p class="mb40 pl15 pr15 explain_60">普段から絡めるファン友を気軽に見つけられます。普段からアイドルトークで盛り上がったり、一緒にお茶したりしながら仲良くなればイベントやライブにも誘いやすくなります^^</p>
+                    </div>
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/miryoku_03.png">
+                        <h4 class="fw_b mt20 mb20">アイドルファン特化だから<br>友達や同行者を見つけやすい</h4>
+                        <p class="mb40 pl15 pr15 explain_80">通常のマッチングサービスやイベント同行者募集サービスと異なりアイドルファン特化型のため効率的にファン仲間と繋がれます</p>
+                    </div>
                 </div>
             </section>
             <section>
-                <h4 class="fw_b pt35 mb35 mt0 fs20">誘いやすさにこだわりました</h4>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/invite_01.png">
-                    <h4 class="fw_b mt20 mb20 fs16">互いに興味を持った人とだけ<br>繋がれるから声もかけやすい</h4>
-                    <p class="mb40 pl15 pr15">お互いに「興味あり」になった段階でマッチングのお知らせが届きます。相手も自分に興味を持ってくれているので安心して声をかけられます♬</p>
+                <h4 class="fw_b pt35 mb60 mt60 fs40">誘いやすさにこだわりました</h4>
+                <div class="sp-around">
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/invite_01.png">
+                        <h4 class="fw_b mt20 mb20 fs16">互いに興味を持った人とだけ<br>繋がれるから声もかけやすい</h4>
+                        <p class="mb40 pl15 pr15 explain_50">お互いに「興味あり」になった段階でマッチングのお知らせが届きます。相手も自分に興味を持ってくれているので安心して声をかけられます♬</p>
+                    </div>
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/invite_02.png">
+                        <h4 class="fw_b mt20 mb20 fs16">アイドルファン特化のプロフィールで<br>話のネタに困らない</h4>
+                        <p class="mb40 pl15 pr15 explain_50">「推し」「参加イベント」「活動歴」などアイドルファンに特化したプロフィールのため、プロフィールを見ながら色んな会話を楽しめます</p>
+                    </div>
                 </div>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/invite_02.png">
-                    <h4 class="fw_b mt20 mb20 fs16">アイドルファン特化のプロフィールで<br>話のネタに困らない</h4>
-                    <p class="mb40 pl15 pr15">「推し」「参加イベント」「活動歴」などアイドルファンに特化したプロフィールのため、プロフィールを見ながら色んな会話を楽しめます</p>
-                </div>                                              
             </section>
             <section class="pt20 sepalator_top">
-                <h3 class="fw_b mb35">Peeechの使い方</h3>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/use_01.png">
-                    <h4 class="fw_b mt25 mb50">プロフィール入力後<br>希望に合った人をレコメンド</h4>
+                <h3 class="fw_b mb60 mt60 fs40">Peeechの使い方</h3>
+                <div class="sp-around">
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/use_01.png">
+                        <h4 class="fw_b mt25 mb50">プロフィール入力後<br>希望に合った人をレコメンド</h4>
+                    </div>
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/use_02.png">
+                        <h4 class="fw_b mt25 mb50">興味ありor興味なしを選択</h4>
+                    </div>
+                    <div>
+                        <img class="wd60 img_box_shadow" src="../../images/top/use_03.png">
+                        <h4 class="fw_b mt25 mb30">マッチングしたら<br>メッセージを送ってみよう</h4>
+                    </div>
                 </div>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/use_02.png">
-                    <h4 class="fw_b mt25 mb50">興味ありor興味なしを選択</h4>
-                </div>
-                <div>
-                    <img class="wd60 img_box_shadow" src="../../images/top/use_03.png">
-                    <h4 class="fw_b mt25 mb30">マッチングしたら<br>メッセージを送ってみよう</h4>
-                </div>
-                <div class="text_align_c">
+                <div class="text_align_c disnone">
                     <img class="wd10" src="../../images/icons/arrow.png">
                 </div>
                 <div>
-                    <h4 class="fw_b mt25 mb50 fs16">仲良くなったら一緒にオフ会したり<br>色んなファン活動を楽しもう</h4>
+                    <h4 class="fw_b mt25 mb50 fs30">仲良くなったら一緒にオフ会したり<br>色んなファン活動を楽しもう</h4>
                 </div>
             </section>
             <section class="convert_to_max_size_of_page section_bg_color fc_white pl15 pr15 pb5">
-                <h3 class="fw_b mb35 pt35">Peeechの想い</h3>
-                <p class="fs18 fw_b mb40">アイドルファン・アイドルオタの<br>幸せな瞬間をもっと増やしたい</p>
-                <p class="text_align_c lh30">好きなアイドルのライブで推しメンの輝く姿を見る、レスをもらう推しメンとの握手会で直接応援の言葉をかけたり二人だけの秘密の会話を楽しんだり。</p>
-                <p class="text_align_c lh30">時には遠くまで何時間もかけて行って何時間も並ぶなどアイドルファン活動は大変な部分もあるけれどその先に待つ、楽しくて幸せな瞬間があると思います。</p>
-                <p class="text_align_c lh30">私は以前はよく一人でライブハウスに行くことが多く大好きなアイドルに会える！というのはそれはそれで楽しいのですが、現場によっては馴染みにくかったり、一人で並んでいる最中寂しいな、好きなアイドルについてもっと誰かと共有したいなと感じることがありました。</p>
-                <p class="text_align_c lh30">そんなあるときから、SNSでつながったとあるオタク友達のおかげで仲間達と好きなアイドルについて語り合ったり一緒にライブ同行・握手連番、時には遠征などをするようになりすごく楽しい時間を過ごしたのです。</p>
-                <p class="text_align_c lh30">「アイドルに会いに行く・アイドルとの時間を楽しむ」もちろんそれが大きな目的ではありましたが「仲間たちに会いに行く」というのも一つの楽しみになっていったのです。</p>
+                <h3 class="fw_b mb35 pt35 fs40">Peeechの想い</h3>
+                <p class="fs18 fw_b mb40 fs20">アイドルファン・アイドルオタの<br>幸せな瞬間をもっと増やしたい</p>
+                <p class="text_align_c lh30">好きなアイドルのライブで推しメンの輝く姿を見る、<br>レスをもらう推しメンとの握手会で直接応援の言葉をかけたり二人だけの秘密の会話を楽しんだり。</p>
+                <p class="text_align_c lh30">時には遠くまで何時間もかけて行って何時間も並ぶなどアイドルファン活動は大変な部分もあるけれど、<br>その先に待つ楽しくて幸せな瞬間があると思います。</p>
+                <p class="text_align_c lh30">私は以前はよく一人でライブハウスに行くことが多く大好きなアイドルに会える！というのはそれはそれで楽しいのですが、<br>現場によっては馴染みにくかったり、一人で並んでいる最中寂しいな、好きなアイドルについてもっと誰かと共有したいなと感じることがありました。</p>
+                <p class="text_align_c lh30">そんなあるときから、SNSでつながったとあるオタク友達のおかげで仲間達と好きなアイドルについて語り合ったり一緒にライブ同行・握手連番、<br>時には遠征などをするようになりすごく楽しい時間を過ごしたのです。</p>
+                <p class="text_align_c lh30">「アイドルに会いに行く・アイドルとの時間を楽しむ」もちろんそれが大きな目的ではありましたが<br>「仲間たちに会いに行く」というのも一つの楽しみになっていったのです。</p>
                 <p class="text_align_c lh30">そんな経験から、ファン活動を誰かと一緒にもっと楽しめ幸せな瞬間をより増やせるといいな、と思いこのサービスを個人的に作っています。</p>
                 <p class="text_align_c lh30">もちろん、「ファン活動を一人で楽しむ」というのもいいものだと思います。</p>
                 <p class="text_align_c lh30">ただ、時々、ほんの少しの時間でも「誰かと一緒にその場を楽しむ」…そんな時間もあってもいいのかもしれません。</p>
