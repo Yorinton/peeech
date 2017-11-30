c8b8177a4442a02ad5bdfd007c91474000206ff5
Yorinton <ka2ki.yori@outlook.com>
2017-11-30 09:33:34 +0000

    SSL化手法をLaravelのsecure()メソッドを使う形に変更

--- app/Http/Middleware/RedirectToHttps.php
+++ app/Http/Middleware/RedirectToHttps.php
@@ -15,9 +15,11 @@ class RedirectToHttps
      */
     public function handle($request, Closure $next)
     {
-        if(!$this->is_ssl() && config('app.env') === 'production'){
-            return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-//            header('Location: https://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
+//        if(!$this->is_ssl() && config('app.env') === 'production'){
+//            return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+//        }
+        if (!$request->secure() && env('APP_ENV') === 'production') { // 本番環境のみ常時SSL化する
+            return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
     }
@@ -26,29 +28,29 @@ class RedirectToHttps
      * Determine if the protocol is HTTPS.
      * @return bool
      */
-    public function is_ssl()
-    {
-        if ( isset($_SERVER['HTTPS']) === true ) // Apache
-        {
-            return ( $_SERVER['HTTPS'] === 'on' or $_SERVER['HTTPS'] === '1' );
-        }
-        elseif ( isset($_SERVER['SSL']) === true ) // IIS
-        {
-            return ( $_SERVER['SSL'] === 'on' );
-        }
-        elseif ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) === true ) // Reverse proxy
-        {
-            return ( strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https' );
-        }
-        elseif ( isset($_SERVER['HTTP_X_FORWARDED_PORT']) === true ) // Reverse proxy
-        {
-            return ( $_SERVER['HTTP_X_FORWARDED_PORT'] === '443' );
-        }
-        elseif ( isset($_SERVER['SERVER_PORT']) === true )
-        {
-            return ( $_SERVER['SERVER_PORT'] === '443' );
-        }
-
-        return false;
-    }
+//    public function is_ssl()
+//    {
+//        if ( isset($_SERVER['HTTPS']) === true ) // Apache
+//        {
+//            return ( $_SERVER['HTTPS'] === 'on' or $_SERVER['HTTPS'] === '1' );
+//        }
+//        elseif ( isset($_SERVER['SSL']) === true ) // IIS
+//        {
+//            return ( $_SERVER['SSL'] === 'on' );
+//        }
+//        elseif ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) === true ) // Reverse proxy
+//        {
+//            return ( strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https' );
+//        }
+//        elseif ( isset($_SERVER['HTTP_X_FORWARDED_PORT']) === true ) // Reverse proxy
+//        {
+//            return ( $_SERVER['HTTP_X_FORWARDED_PORT'] === '443' );
+//        }
+//        elseif ( isset($_SERVER['SERVER_PORT']) === true )
+//        {
+//            return ( $_SERVER['SERVER_PORT'] === '443' );
+//        }
+//
+//        return false;
+//    }
 }
